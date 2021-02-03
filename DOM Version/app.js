8601c9aaa14fd463e6f6a3ac95a057886b2bc1fb
sirfrancois <sirfrancois4@gmail.com>
2021-02-03 13:08:06 -0600

    Jquery Stuff

---
+++ DOM Version/app.js
@@ -0,0 +1,6 @@
+
+.button.addeventlisterner.click(function(){
+    alert('button text')
+})
+
+document.body.innerHTML = <button id= "button"> </button>
--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-asdasdasdasdsdasdasd
+
+const $btn = $("#bigButton");
+let msg = 'Im glad youre alive'
+
+
+   
+
+    $('#bigButton').click(function(){
+        alert('msg');
+    });
+
+
+    $('#Button').click(function(){
+    alert('You NEED a Bigger Button!!!')
+
+    $('#bigButton').appendto(body.innerHTML)
+
+    ;
+    $('<p>' + msg + '</p>').appendto(body.innerHTML)
+     
+
+
+let Friends = [Dave, jay,ryan,brian,ant,liek,dj,carter,ben,lebron]
+
+
+
+
+
+
+
+});
+
+//let div = <div></div>
+//$('div').css.hover()
